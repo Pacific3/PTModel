ee5bf1d969e6746494e075a3d6a43c14b96ad922
Oscar Swanros <swanros@gmail.com>
2015-03-25 16:14:06 -0600

    Create PTModel class files

---
+++ Example/PTModel/PTModel.h
@@ -0,0 +1,13 @@
+//
+//  PTModel.h
+//  PTModel
+//
+//  Created by Oscar Swanros on 3/25/15.
+//  Copyright (c) 2015 Oscar Swanros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTModel : NSObject
+
+@end
---
+++ Example/PTModel/PTModel.m
@@ -0,0 +1,13 @@
+//
+//  PTModel.m
+//  PTModel
+//
+//  Created by Oscar Swanros on 3/25/15.
+//  Copyright (c) 2015 Oscar Swanros. All rights reserved.
+//
+
+#import "PTModel.h"
+
+@implementation PTModel
+
+@end
